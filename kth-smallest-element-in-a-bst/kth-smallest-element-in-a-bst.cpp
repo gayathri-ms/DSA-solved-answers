975415abbdabb0725604db6b99b9885051ba17c2
gayathri-ms <57952962+gayathri-ms@users.noreply.github.com>
2021-11-24 21:28:22 +0530

    Update kth-smallest-element-in-a-bst.cpp

--- kth-smallest-element-in-a-bst/kth-smallest-element-in-a-bst.cpp
+++ kth-smallest-element-in-a-bst/kth-smallest-element-in-a-bst.cpp
@@ -9,6 +9,29 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
+//Optimsed Approach
+
+class Solution {
+public:
+    int check(TreeNode* root,int k,int &count)
+    {
+        if(!root)
+            return -1;
+        int l=check(root->left,k,count);
+        if(l!=-1)
+            return l;
+        if(++count==k)
+            return root->val;
+        int r=check(root->right,k,count);
+        return r;
+    }
+    int kthSmallest(TreeNode* root, int k) {
+        int count=0;
+        return check(root,k,count);
+    }
+};
+
+//second Approach
 class Solution {
 public:
     int count=0;
