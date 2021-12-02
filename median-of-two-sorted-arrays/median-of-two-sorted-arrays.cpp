6ed9bc427f30745f421fc0ff38f3997a16c2daa8
gayathri-ms <57952962+gayathri-ms@users.noreply.github.com>
2021-12-02 21:34:58 +0530

    Update median-of-two-sorted-arrays.cpp

--- median-of-two-sorted-arrays/median-of-two-sorted-arrays.cpp
+++ median-of-two-sorted-arrays/median-of-two-sorted-arrays.cpp
@@ -15,25 +15,30 @@ public:
             cut1=(low+high)/2;
             cut2=n-cut1;
             
-            if(cut1==0)
-                l1=INT_MIN;
-            else
-                l1=nums1[cut1-1];
+            l1=(cut1==0) ? INT_MIN : nums1[cut1-1];
+            l2=(cut2==0) ? INT_MIN : nums2[cut2-1];
+            r1=(cut1==n1) ? INT_MAX : nums1[cut1];
+            r2=(cut2==n2) ? INT_MAX : nums2[cut2];
             
-            if(cut2==0)
-                l2=INT_MIN;
-            else
-                l2=nums2[cut2-1];
+//             if(cut1==0)
+//                 l1=INT_MIN;
+//             else
+//                 l1=nums1[cut1-1];
             
-            if(cut1==n1)
-                r1=INT_MAX;
-            else
-                r1=nums1[cut1];
+//             if(cut2==0)
+//                 l2=INT_MIN;
+//             else
+//                 l2=nums2[cut2-1];
             
-            if(cut2==n2)
-                r2=INT_MAX;
-            else
-                r2=nums2[cut2];
+//             if(cut1==n1)
+//                 r1=INT_MAX;
+//             else
+//                 r1=nums1[cut1];
+            
+//             if(cut2==n2)
+//                 r2=INT_MAX;
+//             else
+//                 r2=nums2[cut2];
             
             if(l1<=r2 && l2<=r1)
             {
