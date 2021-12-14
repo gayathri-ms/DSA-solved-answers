8737079f44909ecb71ede36d2582e6a26e5c5e27
gayathri-ms <57952962+gayathri-ms@users.noreply.github.com>
2021-12-14 19:23:00 +0530

    Update longest-substring-without-repeating-characters.cpp

--- longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.cpp
+++ longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.cpp
@@ -1,3 +1,5 @@
+// using unordered_map
+
 class Solution {
 public:
     int lengthOfLongestSubstring(string s) {
@@ -23,6 +25,34 @@ public:
                 j++;
                 maxi=max(maxi,j-i);
             }
+        }
+        return maxi;
+    }
+};
+
+
+// using unordered_set
+
+class Solution {
+public:
+    int lengthOfLongestSubstring(string s) {
+        unordered_set<char> us;
+        int i=0,j=0;
+        int maxi=0;
+        while(j<s.size())
+        {
+            if(us.find(s[j])!=us.end())
+            {    
+                while(us.find(s[j])!=us.end())
+                {
+                    us.erase(s[i]);
+                    i++;
+                }
+            }
+            us.insert(s[j]);
+            j++;
+            maxi=max(maxi,j-i);
+            
         }
         return maxi;
     }
