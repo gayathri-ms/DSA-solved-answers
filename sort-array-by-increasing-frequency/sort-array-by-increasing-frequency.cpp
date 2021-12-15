8ddafa9127b14ebbcd7fe18773c9ca4667338c6e
gayathri-ms <57952962+gayathri-ms@users.noreply.github.com>
2021-12-15 19:40:56 +0530

    Time: 8 ms (72.03%), Space: 11.5 MB (21.85%) - LeetHub

--- sort-array-by-increasing-frequency/sort-array-by-increasing-frequency.cpp
+++ sort-array-by-increasing-frequency/sort-array-by-increasing-frequency.cpp
@@ -1,10 +1,10 @@
-bool compare(pair<int,int> a,pair<int,int> b)
-{
-    if (a.second != b.second)
-        return (a.second < b.second);
-    else
-        return (a.first > b.first);
-}
+// bool compare(pair<int,int> a,pair<int,int> b)
+// {
+//     if (a.second != b.second)
+//         return (a.second < b.second);
+//     else
+//         return (a.first > b.first);
+// }
 class Solution {
 public:
     
@@ -15,20 +15,34 @@ public:
         {
             mp[nums[i]]++;
         }
-        vector<pair<int,int>> v;
-        for(auto& it : mp)
+        // vector<pair<int,int>> v;
+        // for(auto& it : mp)
+        // {
+        //     v.push_back(make_pair(it.first,it.second));
+        // }
+        // sort(v.begin(),v.end(),compare);
+        // for(int i=0;i<v.size();i++)
+        // {
+        //     int j=v[i].second;
+        //     while(j--)
+        //     {
+        //         ans.push_back(v[i].first);
+        //     }
+        // }
+        
+        priority_queue<pair<int,int>,vector<pair<int,int>>,greater<pair<int,int>>> pq;
+        for(auto& it:mp)
         {
-            v.push_back(make_pair(it.first,it.second));
+            pq.push(make_pair(it.second,-1*it.first));
         }
-        sort(v.begin(),v.end(),compare);
-        for(int i=0;i<v.size();i++)
+        while(pq.size())
         {
-            int j=v[i].second;
+            int j=pq.top().first;
             while(j--)
-            {
-                ans.push_back(v[i].first);
-            }
+            { ans.push_back(-1*pq.top().second); }
+            pq.pop();
         }
+        
         return ans;
     }
 };
